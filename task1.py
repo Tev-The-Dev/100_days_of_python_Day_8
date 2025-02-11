f11ae47ee94902fd41f9b75571e8510bbc9b1cd0
Tevyn <tevyn.payne@gmail.com>
2025-02-10 18:07:05 -0600

    first commit

---
+++ task1.py
@@ -0,0 +1,13 @@
+def greet():
+    print('Hello, and welcome')
+    print('This is the coolest thing and most theraputic thing youve done today huh')
+    print('I hope this will make you feel better')
+
+greet()
+
+def greet_with_name(x):
+    print(f"Hello {x}")
+    print(f"How are you doing today {x}")
+    print(f"{x}, I hope things get better for you soon.")
+
+greet_with_name("Tevyn")
---
+++ task2.py
@@ -0,0 +1,6 @@
+def greet_with(name, location):
+    print(f"Hello {name}")
+    print(f"What is it like in {location}")
+
+greet_with("Tevyn", "Euless")
+greet_with(location="Euless, TX", name="Tevyn")
